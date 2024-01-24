b451aa210ff4d07dab0a300f09f6424f68d67854
LeeviSuominen <115700342+LeeviSuominen@users.noreply.github.com>
2024-01-24 14:13:42 +0200

    Camera photo sound added

--- Silent Lake Outpost Proto/Assets/Scripts/ShowItem.cs
+++ Silent Lake Outpost Proto/Assets/Scripts/ShowItem.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ShowItem : MonoBehaviour
 {
     [SerializeField] GameObject camera;
-
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip photoClip;
     private void Update()
     {
+        PlayAudio();
         if (!camera.activeSelf && Input.GetKeyDown(KeyCode.C))
         {
             camera.SetActive(true);
@@ -18,4 +21,18 @@ public class ShowItem : MonoBehaviour
             camera.SetActive(false);
         }
     }
+
+    private void PlayAudio()
+    {
+        if (camera.activeSelf && Input.GetKeyDown(KeyCode.Mouse0)) 
+        {
+            audioSource.PlayOneShot(photoClip);
+        }
+    }
+
+    /*private IEnumerator TurnFlashOff() 
+    {
+        
+    }
+    */
 }
